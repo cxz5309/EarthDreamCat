5cda8c7397aacf559445f5d66e74857ab1a81dd6
cxz5309 <cxz5309@gmail.com>
2019-08-09 21:05:24 +0900

    스테이지2몬스터제작, 콤보버그 수정, 진행도ui하다맘

---
+++ Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class UIManager : MonoBehaviour
+{
+    public Transform stageSliderLeft;
+    public Transform stageSliderRight;
+    public Transform stageSliderPointer;
+
+    private void Start()
+    {
+        stageSliderPointer.position = stageSliderLeft.position;
+    }
+
+    IEnumerator coStageSlider()
+    {
+        yield return new WaitForFixedUpdate();
+    }
+
+    public void SliderFill()
+    {
+        stageSliderPointer.position = Vector2.MoveTowards(stageSliderPointer.transform.position, stageSliderRight.position, Time.deltaTime/NoteManager.instance.endTime);
+    }
+}
